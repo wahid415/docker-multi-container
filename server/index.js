7138a5361351e17c7dd6c51e0172d336a1081547
wahid <mdwahidali122@gmail.com>
2025-07-19 22:55:12 +0530

    updated code with latest versions

--- server/index.js
+++ server/index.js
@@ -28,11 +28,15 @@ pgClient.on("connect", (client) => {
 // Redis Client Setup
 const redis = require("redis");
 const redisClient = redis.createClient({
-  host: keys.redisHost,
-  port: keys.redisPort,
+  url: `redis://${keys.redisHost}:${keys.redisPort}`,
   retry_strategy: () => 1000,
 });
 const redisPublisher = redisClient.duplicate();
+
+(async () => {
+  await redisClient.connect();
+  await redisPublisher.connect();
+})();
 
 // Express route handlers
 
@@ -47,9 +51,8 @@ app.get("/values/all", async (req, res) => {
 });
 
 app.get("/values/current", async (req, res) => {
-  redisClient.hgetall("values", (err, values) => {
-    res.send(values);
-  });
+  const values = await redisClient.hGetAll("values");
+  res.send(values);
 });
 
 app.post("/values", async (req, res) => {
@@ -59,8 +62,8 @@ app.post("/values", async (req, res) => {
     return res.status(422).send("Index too high");
   }
 
-  redisClient.hset("values", index, "Nothing yet!");
-  redisPublisher.publish("insert", index);
+  await redisClient.hSet("values", index, "Nothing yet!");
+  await redisPublisher.publish("insert", index);
   pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
 
   res.send({ working: true });
